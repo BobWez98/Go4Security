474b7316f6abfef5857ec0db50878c4522de8682
Bob Wezelman <bob.wezelman@hotmail.com>
2019-06-18 11:14:54 +0200

    Fixed vulnerability showstock sql injection

--- showStock.php
+++ showStock.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php 
-    include("login.php");
-    include("includes/head.php");
+<?php
+include "login.php";
+include "includes/head.php";
 ?>
 <body>
     <div id="navb">
     <?php
-        include("includes/topbar.php"); 
-        include("includes/navbar.php");
-        
-        include("includes/scripts.php");
-        
-    ?>
+include "includes/topbar.php";
+include "includes/navbar.php";
+
+include "includes/scripts.php";
+
+?>
     </div>
     <div id="all">
         <div id="content">
@@ -20,60 +20,59 @@
                 <div class="container" style="height:48px">
                   <div>
                     <?php
-                    // TODO: items below should be tidied!
-                    
-                    if (isset($_GET['id']) && isset($_GET['number'])){
-                      $idProduct      = $_GET['id'];
-                      $itemsRequested = $_GET['number'];
-                      
-                      $conn = setupDB($dbhost,$dbSelectUsername,$dbSelectPassword);
-                      $query = "SELECT product_id as id, name as n
-                                FROM product
-                               WHERE nrInStock >= " . $itemsRequested . "
-                                 AND product_id = " . $idProduct . "
-                              order by name;";
-					  $query2 = "SELECT product_id as id, name as n
-                                FROM product
-                               WHERE product_id = " . $idProduct . ";";
-                      try {
-                        $stmt = $conn->prepare($query);
-                        $stmt->execute();
-                        
-                        $row = $stmt->fetch();
+// TODO: items below should be tidied!
 
-                        if ($row !== false){
-                          echo "<div>Van het product " . $row['n'] ." is nog voldoende voorraad ($itemsRequested gevraagd) </div>";
-                        }
-                        else{
-						// have to fetch product name, since prev query didnt return a row
-						  $stmt = $conn->prepare($query2);
-						  $stmt->execute();
-						  $row = $stmt->fetch();
-                          echo "<div>Van het product ". $row['n'] ." is niet voldoende voorraad ($itemsRequested gevraagd) </div>";
-                        }
-                        
-                      } catch (Exception $e) {
-                        echo "<div>SQL Foutmelding: " . $e->getMessage();
-                      }
-                    }//if $_POST parameters present
-                    else{
-                      echo "<div>U heeft geen product en aantal opgegeven.</div>";
-                    }
-                    
-                    ?>
+if (isset($_GET['id']) && isset($_GET['number'])) {
+    $idProduct      = $_GET['id'];
+    $itemsRequested = $_GET['number'];
+
+    $conn = setupDB($dbhost, $dbSelectUsername, $dbSelectPassword);
+    $stmt = $conn->prepare("SELECT product_id as id, name as n
+                                FROM product
+                               WHERE nrInStock >= :items
+                                 AND product_id =:product
+                              order by name;");
+    $stmt2 = $conn->prepare("SELECT product_id as id, name as n
+                                FROM product
+                               WHERE product_id = :product;");
+    try {
+        $stmt->bindParam(':product', $idProduct);
+        $stmt->bindParam(':items', $itemsRequested);
+        $stmt->execute();
+
+        $row = $stmt->fetch();
+
+        if ($row !== false) {
+            echo "<div>Van het product " . $row['n'] . " is nog voldoende voorraad ($itemsRequested gevraagd) </div>";
+        } else {
+            $stmt2->bindParam(':product', $idProduct);
+            $stmt2->execute();
+            $row = $stmt2->fetch();
+            echo "<div>Van het product " . $row['n'] . " is niet voldoende voorraad ($itemsRequested gevraagd) </div>";
+        }
+
+    } catch (Exception $e) {
+        echo "<div>SQL Foutmelding: " . $e->getMessage();
+    }
+} //if $_POST parameters present
+else {
+    echo "<div>U heeft geen product en aantal opgegeven.</div>";
+}
+
+?>
                   </div>
                 </div>
             </div>
             <?php
-                include("includes/index/advantages.php");
-                include("includes/index/hotproducts.php"); 
-            ?>
+include "includes/index/advantages.php";
+include "includes/index/hotproducts.php";
+?>
         </div>
             <?php
-                include("includes/footer.php");
-                include("includes/copyright.php");
-            ?>
+include "includes/footer.php";
+include "includes/copyright.php";
+?>
     </div>
-    <?php include("includes/scripts.php"); ?>
+    <?php include "includes/scripts.php";?>
 </body>
 </html>
