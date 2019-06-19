bd62ca0706a6c418b5da3be40abc072a67c1a62c
Bob Wezelman <bob.wezelman@hotmail.com>
2019-06-19 12:31:38 +0200

    Fix for path traversal terms

--- termsofuse.php
+++ termsofuse.php
@@ -2,15 +2,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-include("login.php");
-include("includes/head.php");
+include "login.php";
+include "includes/head.php";
 ?>
 <body>
 <div id="navb">
     <?php
-    include("includes/topbar.php");
-    include("includes/navbar.php");
-    ?>
+include "includes/topbar.php";
+include "includes/navbar.php";
+?>
 </div>
 <div id="all">
     <div id="content">
@@ -20,30 +20,22 @@ include("includes/head.php");
                     <div class="col-md-11">
                         <h2>Terms of use</h2>
                         <?php
-                            $url = $_SERVER['REQUEST_URI'];
-                            if ((strpos($url,"file")&&(strpos($url,".txt")) !== false)) {
-                               if (file_exists((string)($_GET['file']))){
-                                   $test = file_get_contents($_GET['file']);
-                                   echo "<pre>$test</pre>";
-                               }
-                               else {
-                                   echo "<strong>file does not exist</strong>";
-                               }
-                            }
-                            else{
-                                echo "<strong>couldn't find .txt file</strong>";
-                            }
-                        ?>
+$url = $_SERVER['REQUEST_URI'];
+if (file_exists("terms.txt")) {
+    $terms = file_get_contents("terms.txt");
+    echo "<pre>$terms</pre>";
+}
+?>
         </div>
     </div>
 </div>
 
     </div>
     <?php
-    include("includes/footer.php");
-    include("includes/copyright.php");
-    ?>
+include "includes/footer.php";
+include "includes/copyright.php";
+?>
 </div>
-<?php include("includes/scripts.php"); ?>
+<?php include "includes/scripts.php";?>
 </body>
 </html>
