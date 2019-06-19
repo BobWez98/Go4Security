736ac9b0ff482927f0011a4bdc67397a59674d10
Bob Wezelman <bob.wezelman@hotmail.com>
2019-06-19 11:50:46 +0200

    Fixed bypassing password check

--- login.php
+++ login.php
@@ -1,57 +1,56 @@
 <?php
-    session_start();
-    include("dbConfig.php");
-    $user = $pass = $userEr = $passEr = $err ="";
-    $admin = "ADM1n";
+session_start();
+include "dbConfig.php";
+$user  = $pass  = $userEr  = $passEr  = $err  = "";
+$admin = "ADM1n";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && (!isset($_SESSION['user']))){ 
-        $conn = setupDB($dbhost,$dbSelectUsername,$dbSelectPassword);             
-        if(!empty($_POST["user"])){
-            $user = $_POST["user"];
-        }
-        if(!empty($_POST["pass"])){
-            $pass = $_POST["pass"];
-        }
-        if(!empty($user) && !empty($pass)){
-            login($conn,$user,$pass); 
-        }
+if ($_SERVER["REQUEST_METHOD"] == "POST" && (!isset($_SESSION['user']))) {
+    $conn = setupDB($dbhost, $dbSelectUsername, $dbSelectPassword);
+    if (!empty($_POST["user"])) {
+        $user = $_POST["user"];
     }
-	
-	if (!isset($_SESSION['user'])){
-		// backdoor <auto login > during development. !! remove before going live !!
-		// set cookie to user%password
-		if(isset($_COOKIE["g4sBackDoor"]) && strpos($_COOKIE["g4sBackDoor"], "%")) {	
-			list($user,$pass) = explode("%",$_COOKIE["g4sBackDoor"], 2);
-			$conn = setupDB($dbhost,$dbSelectUsername,$dbSelectPassword);	
-			login($conn,$user,$pass);
-		}
-	}
+    if (!empty($_POST["pass"])) {
+        $pass = $_POST["pass"];
+    }
+    if (!empty($user) && !empty($pass)) {
+        login($conn, $user, $pass);
+    }
+}
 
-    function login($conn,$user,$pass){
-        global $admin;
-        $pass = hash("sha256",$pass); //simpele hash functie zonder salt      
-        try{
-            $res = $conn->query("SELECT * FROM user WHERE username = '{$user}' AND pass = '{$pass}';");
-            $row = $res->fetch();
-            if($row){
-                $_SESSION['user'] = $row[1];
-                $res = null;
-                $conn = null; 
-                if($row[1] === $admin){          
-                    header("location: admin.php");
-                    exit();
-                }
-                else {
-                    header("location: index.php");
-                    exit();
-                }
-            }
-            else{
-                echo "Username or password invalid!"; 
-            }
-        }
-        catch(PDOException $e){
-            echo "Login error";
-        }
+if (!isset($_SESSION['user'])) {
+    // backdoor <auto login > during development. !! remove before going live !!
+    // set cookie to user%password
+    if (isset($_COOKIE["g4sBackDoor"]) && strpos($_COOKIE["g4sBackDoor"], "%")) {
+        list($user, $pass) = explode("%", $_COOKIE["g4sBackDoor"], 2);
+        $conn              = setupDB($dbhost, $dbSelectUsername, $dbSelectPassword);
+        login($conn, $user, $pass);
     }
-?>
+}
+
+function login($conn, $user, $pass)
+{
+    global $admin;
+    $pass = hash("sha256", $pass); //simpele hash functie zonder salt
+    try {
+        $res = $conn->prepare("SELECT * FROM user WHERE username = ':username' AND pass = ':pass';");
+        $res->bindParam(':username', $user);
+        $res->bindParam(':pass', $pass);
+        $row = $res->fetch();
+        if ($row) {
+            $_SESSION['user'] = $row[1];
+            $res              = null;
+            $conn             = null;
+            if ($row[1] === $admin) {
+                header("location: admin.php");
+                exit();
+            } else {
+                header("location: index.php");
+                exit();
+            }
+        } else {
+            echo "Username or password invalid!";
+        }
+    } catch (PDOException $e) {
+        echo "Login error";
+    }
+}
